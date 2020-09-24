6700514412c19918ee4be62158623b0a415296eb
Mabroor Ahmad <amabroor007@gmail.com>
2020-09-24 11:53:15 +0500

    QraphQl set and added a first param based Query

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs } = require("./typeDefs");
+const { resolvers } = require("./resolvers");
+
+// app
+const app = express();
+
+// Graphql server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Applying Graphql middleware
+server.applyMiddleware({ app });
+
+app.listen(4000, () => {
+  console.log("Listening on port 4000...");
+});
